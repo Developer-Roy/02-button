bcf6a60252454bf53401346108a19894f86f1724
Rohit Roy <rohitroy62945@gmail.com>
2026-07-28 08:50:44 +0530

    add more code that look good button

--- script.js
+++ script.js
@@ -6,25 +6,17 @@ const callImage = document.getElementById("call-img");
 const text = document.getElementById("text");
 
 
-// button.addEventListener("mouseenter", () => {
-//     console.log("it's activated")
-//     profileImage.classList.add("activeProfile");
-//     callImage.classList.add("activeCall");
-//     text.classList.add('activeText');
-// })
+const hoverSound = new Audio("./assets/sounds/ipadclick.mp3");
 
-// button.addEventListener("mouseleave", () => {
-//     console.log("it's removed");
-//     profileImage.classList.remove("activeProfile");
-//     callImage.classList.remove("activeCall");
-//     text.classList.remove('activeText');
-// })
+hoverSound.volume = 0.1;
+hoverSound.preload = "auto";
+
 
 
 const tl = gsap.timeline({ paused: true });
 
 tl.to(text,{
-    x: 50,
+    x: 0,
     opacity:1,
     zIndex:0,
     duration:0.2
@@ -34,7 +26,7 @@ tl.to(text,{
     x:10,
     rotation:360,
     filter:"blur(0px)",
-    duration:0.6,
+    duration:0.3,
     zIndex:1
 },0)
 .to(callImage,{
@@ -42,14 +34,20 @@ tl.to(text,{
     filter:"blur(0px)",
     x:-90,
     rotation:-360,
-    duration:0.6,
+    duration:0.3,
     zIndex:1000
 },0);
 
 button.addEventListener("mouseenter",()=>{
     tl.play();
+
+    
 });
 
 button.addEventListener("mouseleave",()=>{
     tl.reverse();
+
 });
+
+
+
